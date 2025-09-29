ba55c00a566094da1119d60de57bdfb173aa27f1
hailong289 <longdh2.dev@gmail.com>
2025-09-29 15:43:39 +0700

    feat(kafka): streamline Kafka topic provisioning and remove unused code

--- libs/scripts/create-topics.ts
+++ libs/scripts/create-topics.ts
@@ -4,34 +4,28 @@ import { Kafka } from 'kafkajs';
 async function main() {
     try {
         console.log('🚀 Starting Kafka topic provisioning...');
-        const kafkaAuth = new Kafka({
-            clientId: 'auth-service',
-            brokers: [process.env.KAFKA_BROKER || 'localhost:9092'],
-        });
-        const kafkaFilesystem = new Kafka({
+        const kafka = new Kafka({
             clientId: 'filesystem-service',
             brokers: [process.env.KAFKA_BROKER || 'localhost:9092'],
             
         });
 
-        const adminAuth = kafkaAuth.admin();
-        await adminAuth.connect();
-        const adminFilesystem = kafkaFilesystem.admin();
-        await adminFilesystem.connect();
+        const admin = kafka.admin();
+        await admin.connect();
 
 
-        const topicsAuthToCreate = [
-            { topic: 'login', numPartitions: 1, replicationFactor: 1 },
-            { topic: 'login.reply', numPartitions: 1, replicationFactor: 1 },
-            { topic: 'register', numPartitions: 1, replicationFactor: 1 },
-            { topic: 'register.reply', numPartitions: 1, replicationFactor: 1 },
-            { topic: 'logout', numPartitions: 1, replicationFactor: 1 },
-            { topic: 'logout.reply', numPartitions: 1, replicationFactor: 1 },
-            { topic: 'get_user', numPartitions: 1, replicationFactor: 1 },
-            { topic: 'get_user.reply', numPartitions: 1, replicationFactor: 1 },
-        ];
+        // const topicsAuthToCreate = [
+        //     { topic: 'login', numPartitions: 1, replicationFactor: 1 },
+        //     { topic: 'login.reply', numPartitions: 1, replicationFactor: 1 },
+        //     { topic: 'register', numPartitions: 1, replicationFactor: 1 },
+        //     { topic: 'register.reply', numPartitions: 1, replicationFactor: 1 },
+        //     { topic: 'logout', numPartitions: 1, replicationFactor: 1 },
+        //     { topic: 'logout.reply', numPartitions: 1, replicationFactor: 1 },
+        //     { topic: 'get_user', numPartitions: 1, replicationFactor: 1 },
+        //     { topic: 'get_user.reply', numPartitions: 1, replicationFactor: 1 },
+        // ];
 
-        const topicsFilesystemToCreate = [
+        const topicsToCreate = [
             { topic: 'upload_single_file', numPartitions: 1, replicationFactor: 1 },
             { topic: 'upload_single_file.reply', numPartitions: 1, replicationFactor: 1 },
             { topic: 'upload_multiple_files', numPartitions: 1, replicationFactor: 1 },
@@ -42,12 +36,9 @@ async function main() {
             { topic: 'get_presigned_url.reply', numPartitions: 1, replicationFactor: 1 },
         ];
 
-        await adminAuth.createTopics({ topics: topicsAuthToCreate });
-        await adminFilesystem.createTopics({ topics: topicsFilesystemToCreate });
+        await admin.createTopics({ topics: topicsToCreate });
         console.log('✅ Kafka topics provisioned successfully');
-
-        await adminAuth.disconnect();
-        await adminFilesystem.disconnect();
+        await admin.disconnect();
     } catch (error) {
         console.error('❌ Error creating topics', error);
     }
