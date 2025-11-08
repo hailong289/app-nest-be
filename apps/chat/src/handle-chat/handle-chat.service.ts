9c0dd827087fd1a1b2bc135d84efbbebd2930bd3
Lê Thiên Trí <thientrile2003@gmail.com>
2025-11-08 20:28:29 +0700

    fix: Log message data before creating a new message in HandleChatService

--- apps/chat/src/handle-chat/handle-chat.service.ts
+++ apps/chat/src/handle-chat/handle-chat.service.ts
@@ -87,6 +87,7 @@ export class HandleChatService {
     if (id) {
       data._id = this.utils.convertToObjectIdMongoose(id);
     }
+    this.log.log(data);
     // create new message (without transaction for standalone MongoDB)
     const createNewMsg = await this.messageModel.create(data);
     if (!createNewMsg) {
