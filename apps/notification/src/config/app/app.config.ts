9d967910944eb4d5595923dc287d5bacac43dce1
Hải Long <longdh2.dev@gmail.com>
2026-05-09 15:31:01 +0700

    fix: env

--- apps/notification/src/config/app/app.config.ts
+++ apps/notification/src/config/app/app.config.ts
@@ -2,6 +2,6 @@ import { registerAs } from '@nestjs/config';
 
 export default registerAs('app', () => {
   return {
-    url_frontend: process.env.APP_URL_FRONTEND,
+    url_frontend: process.env.URL_FRONTEND || process.env.APP_URL_FRONTEND || 'http://localhost:3000',
   };
 });
